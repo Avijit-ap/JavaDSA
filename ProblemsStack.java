e0b33bbead04d5957a332c5e6f9610a71e1bff32
Avijit <jitavi185@gmail.com>
2023-09-18 17:03:37 +0530

    Stack and queue file push

---
+++ ProblemsStack.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+/*Create a stack and push each character of the string onto the stack.
+Create a new string and pop each character from the stack and append it to the new string.
+Return the new string. */
+public class ProblemsStack {
+
+    public static void main(String[] args) {
+        String str="Hello, world!";
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i=0; i<str.length();i++){
+            stack.push(str.charAt(i));
+        }
+        String reverse="";
+        while(!stack.empty()){
+            reverse=reverse+stack.pop();
+        }
+        System.out.println(reverse);
+
+    }
+}
---
+++ stackMain.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+public class stackMain {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        /*There are 5 Java Stack function available in Stack class which are:
+            push(), 
+            pop(), 
+            peek(), 
+            search(), 
+            empty()
+        */
+
+        // Pushing elements in the stack
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+
+        // Peeking the top element of the stack
+        String element = stack.peek();
+        System.out.println("Top element is: " + element);
+
+        // Popping the top element of the stack
+         stack.pop();
+
+        // Searching for an element in the stack
+        System.out.println("The position of element A in the stack is: " + stack.search("A"));
+        System.out.println("The position of element D in the stack is: " + stack.search("D"));
+
+       //Store the poped item 
+         String popedItem = stack.pop();
+            System.out.println("Poped item is: " + popedItem);
+        
+        /*Adding item through for loop in a stack
+
+            for(int i=0;i<=4;i++){
+                stack.push("E");
+            }
+            System.out.println("Stack is: " + stack);*/
+
+
+    }
+}
